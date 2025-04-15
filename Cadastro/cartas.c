3de8e4e520812d5b03a0356ca207bf403f98aa4e
Ronald2025santos <alvesronald2333@gmail.com>
2025-04-15 11:41:00 +0000

    Cadastrodascartas

---
+++ Cadastro/cartas.c
@@ -0,0 +1,31 @@
+    #include <stdio.h>
+
+
+        int main(){
+         char Estado;
+         char Código da carta {A01};
+         char Nome da cidade {Fortaleza};
+         int População;
+         float Área em km²
+         float PIB;
+         int Número de pontos turísticos;
+
+
+        printf ("Desafio Super Trunfo\n");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
